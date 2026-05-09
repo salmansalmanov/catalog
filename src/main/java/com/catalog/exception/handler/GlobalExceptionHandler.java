b0c5b641e45b8976937edf5173f1dd806d2ca2c9
salmansalmanov <salmanov2904@gmail.com>
2026-05-09 12:00:17 +0400

    feat: add exception handling for validation and internal server errors

--- src/main/java/com/catalog/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/catalog/exception/handler/GlobalExceptionHandler.java
@@ -3,11 +3,17 @@ package com.catalog.exception.handler;
 import com.catalog.exception.custom.AlreadyExistsException;
 import com.catalog.exception.custom.InvalidTokenException;
 import com.catalog.exception.custom.NotFoundException;
+import com.catalog.model.result.ErrorDataResult;
 import com.catalog.model.result.ErrorResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -31,5 +37,19 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new ErrorResult(ex.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDataResult<Map<String, String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return new ResponseEntity<>(new ErrorDataResult<>(errors, "Validation failed"), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResult> handleException(Exception ex) {
+        return new ResponseEntity<>(new ErrorResult("An unexpected error occurred: " + ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
